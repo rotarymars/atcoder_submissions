07dcd0ac81b5e4c0ce29cc874bf51e77f1a9cab6
runner <runner@fv-az1991-121>
2025-04-06 14:24:21 +0000

    Add abc161/abc161_a/abc161_a.cpp

---
+++ abc161/abc161_a/abc161_a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<c<<' '<<a<<' '<<b<<endl;
+    return 0;
+}
