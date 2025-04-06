683bb0d68f3e1f32eeadcfa8b6974899d594b347
runner <runner@fv-az1043-500>
2025-04-06 14:07:41 +0000

    Add math-and-algorithm/math_and_algorithm_a/math_and_algorithm_a.cpp

---
+++ math-and-algorithm/math_and_algorithm_a/math_and_algorithm_a.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    // write code here
+    int N;
+    cin >> N;
+    cout << 5 + N << endl;
+    return 0;
+}
