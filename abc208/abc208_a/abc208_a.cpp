53ccf1ff6dbb5a38ed1cfb610e207b180756d321
runner <runner@fv-az1991-121>
2025-04-06 14:24:56 +0000

    Add abc208/abc208_a/abc208_a.cpp

---
+++ abc208/abc208_a/abc208_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int a,b;
+    cin>>a>>b;
+    if(1*a<=b&&b<=6*a)cout<<"Yes\n";
+    else cout<<"No\n";
+    return 0;
+}
