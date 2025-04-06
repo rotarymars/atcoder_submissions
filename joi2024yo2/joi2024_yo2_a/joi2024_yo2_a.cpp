0b5075e9badb7af8d819bec72583129b99afb061
runner <runner@fv-az1671-830>
2025-04-06 14:16:17 +0000

    Add joi2024yo2/joi2024_yo2_a/joi2024_yo2_a.cpp

--- joi2024yo2/joi2024_yo2_a/joi2024_yo2_a.cpp
+++ joi2024yo2/joi2024_yo2_a/joi2024_yo2_a.cpp
@@ -3,14 +3,22 @@ using namespace std;
 int main(){
   int n;
   cin >> n;
+  int arraymaxelement = 0;
   vector<int> a(n);
   for (auto &i : a){
     cin >> i;
+    arraymaxelement = max(arraymaxelement, i);
   }
-  sort(a.begin(), a.end());
-  a.erase(unique(a.begin(), a.end()), a.end());
-  for (auto i = a.begin(); i != a.end(); i++){
-    if ((*lower_bound(a.begin(), a.end(), (*i) + 3) == ((*i) + 3)) && (*lower_bound(a.begin(), a.end(), (*i) + 6) == ((*i) + 6))){cout << "Yes\n"; return 0;}
+  vector<bool> exist(arraymaxelement + 1, false);
+  for (auto &i : a){
+    exist[i] = true;
+  }
+  for (auto &i : a){
+    if (exist[i] && exist[i + 3] && exist[i + 6]){
+      cout << "Yes\n";
+      return 0;
+    }
   }
   cout << "No\n";
+  return 0;
 }
