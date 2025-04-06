086faa87fec2dc6044dafd4c6f4b297bd5de5702
runner <runner@fv-az1043-500>
2025-04-06 14:07:50 +0000

    Add joi2023yo1a/joi2023_yo1a_b/joi2023_yo1a_b.cpp

--- joi2023yo1a/joi2023_yo1a_b/joi2023_yo1a_b.cpp
+++ joi2023yo1a/joi2023_yo1a_b/joi2023_yo1a_b.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-
-    // write code here
-    int n;
-    cin >> n;
-    if (n / 10 == n % 10) {
-        cout << 1 << endl;
-    }
-    else {
-        cout << 0 << endl;
-    }
-    return 0;
+int main() {
+	int n;
+	cin >> n;
+	if (n / 10 == n % 10) {
+		cout << 1 << "\n";
+	}
+	else {
+		cout << 0 << "\n";
+	}
 }
