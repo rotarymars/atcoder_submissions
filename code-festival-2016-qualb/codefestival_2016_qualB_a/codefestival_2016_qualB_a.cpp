8729766bf2b0d62010729521f2260bf04f7541c2
runner <runner@fv-az1937-505>
2025-04-06 15:08:26 +0000

    Add code-festival-2016-qualb/codefestival_2016_qualB_a/codefestival_2016_qualB_a.cpp

---
+++ code-festival-2016-qualb/codefestival_2016_qualB_a/codefestival_2016_qualB_a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  char a[] = "CODEFESTIVAL2016", b[100];
+  scanf("%s", b);
+  int ans = 0;
+  for (int i = 0; i < 16; i++)
+    if (a[i] != b[i])
+      ans++;
+  cout << ans << endl;
+  return 0;
+}
+
+/*
+                          ./+o+-
+                  yyyyy- -yyyyyy+
+               ://+//////-yyyyyyo
+           .++ .:/++++++/-.+sss/`
+         .:++o:  /++++++++/:--:/-
+        o:+o+:++.`..```.-/oo+++++/
+       .:+o:+o/.          `+sssoo+/
+  .++/+:+oo+o:`             /sssooo.
+ /+++//+:`oo+o               /::--:.
+ \+/+o+++`o++o               ++////.
+  .++.o+++oo+:`             /dddhhh.
+       .+.o+oo:.          `oddhhhh+
+        \+.++o+o``-````.:ohdhhhhh+
+         `:o+++ `ohhhhhhhhyo++os:
+           .o:`.syhhhhhhh/.oo++o`
+               /osyyyyyyo++ooo+++/
+                   ````` +oo+++o\:
+                          `oo++.
+*/
