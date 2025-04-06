21f55fc25d47b43b0d2f3864e0e9e75dfc642d87
runner <runner@fv-az1043-500>
2025-04-06 14:09:24 +0000

    Add joi2023yo1c/joi2023_yo1c_c/joi2023_yo1c_c.cpp

--- joi2023yo1c/joi2023_yo1c_c/joi2023_yo1c_c.cpp
+++ joi2023yo1c/joi2023_yo1c_c/joi2023_yo1c_c.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 int main(){
   int n;
   string s;
   cin >> n >> s;
-  vector <char> s_1(s.begin(), s.end());
-  for(int i = 0; i < n - 1; ++i){
-    if(s_1[i] + s_1[i + 1] == 'j' + 'j'){
-      s_1[i] = 'J';
-      s_1[i + 1] = 'J';
-    }else if(s_1[i] + s_1[i + 1] == 'o' + 'o'){
-      s_1[i] = 'O';
-      s_1[i + 1] = 'O';
-    }else if(s_1[i] + s_1[i + 1] == 'i' + 'i'){
-      s_1[i] = 'I';
-      s_1[i + 1] = 'I';
+  for (int i = 0; i < n - 1; i++){
+    if (s[i] == s[i + 1]){
+      s[i] = s[i] - ('a' - 'A');
+      s[i + 1] = s[i + 1] - ('a' - 'A');
     }
   }
-  for(int i = 0; i < n; ++i){
-    cout << s_1[i];
-  }
+  cout << s << endl;
 }
