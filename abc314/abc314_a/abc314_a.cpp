dcc426a6a876593a009afe81d791bd665177ab10
runner <runner@fv-az1671-830>
2025-04-06 14:17:57 +0000

    Add abc314/abc314_a/abc314_a.cpp

--- abc314/abc314_a/abc314_a.cpp
+++ abc314/abc314_a/abc314_a.cpp
@@ -1,8 +1,9 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-  string s = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
-  int n;
-  cin >> n;
-  cout << s.substr(0, n + 2);
+#include <stdio.h>
+int main() {
+  char tmp;
+  int a = 0;
+  constexpr char* b = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+  while ((tmp = getchar()) != '\n') a *= 10, a += (tmp - '0');
+  putchar('3'); putchar('.');
+  for (int i = 2; i < a + 2; i++) putchar(b[i]);
 }
