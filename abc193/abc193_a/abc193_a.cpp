13a5b9b6c2b6a676db5ed21c809d39263fc9bd24
runner <runner@fv-az1991-121>
2025-04-06 14:24:20 +0000

    Add abc193/abc193_a/abc193_a.cpp

---
+++ abc193/abc193_a/abc193_a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    long double a,b;
+    cin>>a>>b;
+    cout<<(1-b/a)*100<<endl;
+    return 0;
+}
