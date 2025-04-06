03a17c56efc5833f61dd84624dc581a9b7458de3
runner <runner@fv-az1671-830>
2025-04-06 14:16:46 +0000

    Add abc049/abc049_b/abc049_b.cpp

---
+++ abc049/abc049_b/abc049_b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+  int h, w;
+  cin >> h >> w;
+  while (h--){
+    string tmp;
+    cin >> tmp;
+    cout << tmp << "\n" << tmp << "\n";
+  }
+}
