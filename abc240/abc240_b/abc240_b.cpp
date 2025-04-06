1465f562b67d890f172ad14f8786e9a23dc9c243
runner <runner@fv-az1991-121>
2025-04-06 14:25:01 +0000

    Add abc240/abc240_b/abc240_b.cpp

---
+++ abc240/abc240_b/abc240_b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    set<int>se;
+    for(;n;n--){
+        int tmp;
+        cin>>tmp;
+        se.insert(tmp);
+    }
+    cout<<se.size()<<endl;
+    return 0;
+}
