bab592dc3c03b7e035b9ffcb669794142f09599b
runner <runner@fv-az1991-121>
2025-04-06 14:23:30 +0000

    Add joi2020yo1a/joi2020_yo1a_c/joi2020_yo1a_c.cpp

--- joi2020yo1a/joi2020_yo1a_c/joi2020_yo1a_c.cpp
+++ joi2020yo1a/joi2020_yo1a_c/joi2020_yo1a_c.cpp
@@ -1,22 +1,12 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 int main(){
   int n, m;
   cin >> n >> m;
-  vector<int> a(n);
-  vector<int> b(m);
-  vector<int> c;
-  for(int i = 0; i < n; ++i){
-    cin >> a[i];
-  }
-  for(int i = 0; i < m; ++i){
-    cin >> b[i];
-  }
-  a.insert(a.end(), b.begin(), b.end());
+  vector<int> a(n + m);
+  for (int &i : a) cin >> i;
   sort(a.begin(), a.end());
-  for(int i = 0; i < n + m; ++i){
-    cout << a[i] << "\n";
-  }
+  for (int &i : a) cout << i << "\n";
 }
