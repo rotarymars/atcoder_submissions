0adf9fedd6cc3dc04cb5918537ee04d2ef09e5d1
runner <runner@fv-az1671-830>
2025-04-06 14:16:20 +0000

    Add abc321/abc321_a/abc321_a.cpp

--- abc321/abc321_a/abc321_a.cpp
+++ abc321/abc321_a/abc321_a.cpp
@@ -1,42 +1,18 @@
-﻿#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-  cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-
-  // write code here
-  int n, length, m, x = 0, out = 0;
-  string str_n;
+int main(){
+  int n;
   cin >> n;
-  str_n = to_string(n);
-  length = str_n.size();
-  m = str_n[0];
-  if (length == 1) {
-    cout << "Yes" << endl;
-    out = 1;
-  }
-  else {
-    for (size_t i = 1; i < length; ++i)
-    {
-      if ((int)str_n[i] < m) {
-        m = str_n[i];
-      }
-      else {
-        x = 1;
-        cout << "No" << endl;
-        out = 1;
-        break;
-      }
+  int backnum = n % 10;
+  n /= 10;
+  while (n){
+    if (backnum < n % 10){
+      backnum = n % 10;
+      n /= 10;
+      continue;
     }
+    cout << "No\n";
+    return 0;
   }
-  if (out == 0) {
-    cout << "Yes" << endl;
-  }
-  return 0;
+  cout << "Yes\n";
 }
