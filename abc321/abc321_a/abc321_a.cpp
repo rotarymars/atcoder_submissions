d88c947c14103f90dc0527a76d66de04ae4a6013
runner <runner@fv-az1043-500>
2025-04-06 14:08:05 +0000

    Add abc321/abc321_a/abc321_a.cpp

--- abc321/abc321_a/abc321_a.cpp
+++ abc321/abc321_a/abc321_a.cpp
@@ -1,4 +1,4 @@
-#include <algorithm>
+﻿#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,36 +7,36 @@ using namespace std;
 
 int main()
 {
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
 
-    // write code here
-    int n, length, m, x = 0, out = 0;
-    string str_n;
-    cin >> n;
-    str_n = to_string(n);
-    length = str_n.size();
-    m = str_n[0];
-    if (length == 1) {
-        cout << "Yes" << endl;
+  // write code here
+  int n, length, m, x = 0, out = 0;
+  string str_n;
+  cin >> n;
+  str_n = to_string(n);
+  length = str_n.size();
+  m = str_n[0];
+  if (length == 1) {
+    cout << "Yes" << endl;
+    out = 1;
+  }
+  else {
+    for (size_t i = 1; i < length; ++i)
+    {
+      if ((int)str_n[i] < m) {
+        m = str_n[i];
+      }
+      else {
+        x = 1;
+        cout << "No" << endl;
         out = 1;
+        break;
+      }
     }
-    else {
-        for (size_t i = 1; i < length; ++i)
-        {
-            if ((int)str_n[i] < m) {
-                m = str_n[i];
-            }
-            else {
-                x = 1;
-                cout << "No" << endl;
-                out = 1;
-                break;
-            }
-        }
-    }
-    if (out == 0) {
-        cout << "Yes" << endl;
-    }
-    return 0;
+  }
+  if (out == 0) {
+    cout << "Yes" << endl;
+  }
+  return 0;
 }
