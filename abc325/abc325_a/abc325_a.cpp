48614fea9c074a50f7ab0f715db7b6d8727db8f8
runner <runner@fv-az1043-500>
2025-04-06 14:07:56 +0000

    Add abc325/abc325_a/abc325_a.cpp

---
+++ abc325/abc325_a/abc325_a.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  // write code here
+  string a, b;
+  cin >> a >> b;
+  cout << a << " san\n";
+  return 0;
+}
