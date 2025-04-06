dcd43d3a25edcf6c2901ba531bce0adefa163ee0
runner <runner@fv-az1937-505>
2025-04-06 15:09:14 +0000

    Add code-festival-2014-qualb/code_festival_qualB_b/code_festival_qualB_b.cpp

---
+++ code-festival-2014-qualb/code_festival_qualB_b/code_festival_qualB_b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  long long n, k;
+  cin >> n >> k;
+  long long now = 0;
+  for (int i = 0; i < n; i++) {
+    long long a;
+    cin >> a;
+    now += a;
+    if (now >= k) {
+      cout << i + 1 << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
+
+/*
+
+                   N~
+   MMMMMMMMMMMMMMM+O$M MMMMMMMMMMMMMMM+
+  :M~~~~~~~~~~~~OMOOON$M,~~~~~~~~~~~~IM
+  :M$77~~~~~~~77IMOOOOOM7I7~~~~~~~~~I8M
+     M7~~~~~~~IMOOOOOOOOM8 ~~~~~~~:IM
+     M7~~~~~~~IMOOOOOO8M +~~~~~~~IMM
+     M7~~~~~~~IMOOOOOM. ~~~~~~~DIM
+     M7~~~~~~~IMOOOMM :~~~~~~~IMM
+     M7~~~~~~~IMOOM :~~~~~~~NIM$N
+    MM7~~~~~~~IMMO ~~~~~~~~IMMOOO$M
+   7+M7~~~~~~~IM O~~~~~~~ONMOOOOOOZ$.
+ M+OOM7~~~~~~~I ~~~~~~~:IMOOOOOOOOOO$M
+ M8OOM7~~~~~~~?~~~~~~~7MMOOOOOOOOOOODM
+  88NM7~~~~~~~~~~~~~MMMOOOOOOOOOOOOD+
+    MM7~~~~~~~~~~~~M~~MOOOOOOOOOODM
+     M7~~~~~~~~~~=IMOOOOOOOOOOONN
+     M7~~~~~~~~~I?~~~MM~~~MD+~~M8~~~$
+     M7~~~~~~~IZMD~~ZOO:~~~~~~~~~~~~.
+     M7~~~~~~IMDO8~~NOM~~NDM~~M ~~~M
+     M7~~~~ZNMOOM~~8OO:~?DM8~~  M~~?
+     M7~~~IM.:ND:~~MOM~~M M~~M N~~O~
+     MMMMMM    MMMMMODNM+ ?III +III
+                 M8ODM
+                  M8O
+
+*/
