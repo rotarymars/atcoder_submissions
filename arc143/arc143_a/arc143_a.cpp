04a13da1dc53baaacbc2f506bfa99a6b47989d3a
runner <runner@fv-az1937-505>
2025-04-06 15:08:45 +0000

    Add arc143/arc143_a/arc143_a.cpp

---
+++ arc143/arc143_a/arc143_a.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  vector<long long> v(3);
+  cin >> v[0] >> v[1] >> v[2];
+  sort(v.begin(), v.end());
+  if (v[2] >= 3ll * v[2] - v[0] - v[1] - v[2])
+    cout << v[2] << endl;
+  else
+    cout << "-1\n";
+  return 0;
+}
+
+/*
+
+                   N~
+   MMMMMMMMMMMMMMM+O$M MMMMMMMMMMMMMMM+
+  :M~~~~~~~~~~~~OMOOON$M,~~~~~~~~~~~~IM
+  :M$77~~~~~~~77IMOOOOOM7I7~~~~~~~~~I8M
+     M7~~~~~~~IMOOOOOOOOM8 ~~~~~~~:IM
+     M7~~~~~~~IMOOOOOO8M +~~~~~~~IMM
+     M7~~~~~~~IMOOOOOM. ~~~~~~~DIM
+     M7~~~~~~~IMOOOMM :~~~~~~~IMM
+     M7~~~~~~~IMOOM :~~~~~~~NIM$N
+    MM7~~~~~~~IMMO ~~~~~~~~IMMOOO$M
+   7+M7~~~~~~~IM O~~~~~~~ONMOOOOOOZ$.
+ M+OOM7~~~~~~~I ~~~~~~~:IMOOOOOOOOOO$M
+ M8OOM7~~~~~~~?~~~~~~~7MMOOOOOOOOOOODM
+  88NM7~~~~~~~~~~~~~MMMOOOOOOOOOOOOD+
+    MM7~~~~~~~~~~~~M~~MOOOOOOOOOODM
+     M7~~~~~~~~~~=IMOOOOOOOOOOONN
+     M7~~~~~~~~~I?~~~MM~~~MD+~~M8~~~$
+     M7~~~~~~~IZMD~~ZOO:~~~~~~~~~~~~.
+     M7~~~~~~IMDO8~~NOM~~NDM~~M ~~~M
+     M7~~~~ZNMOOM~~8OO:~?DM8~~  M~~?
+     M7~~~IM.:ND:~~MOM~~M M~~M N~~O~
+     MMMMMM    MMMMMODNM+ ?III +III
+                 M8ODM
+                  M8O
+
+*/
