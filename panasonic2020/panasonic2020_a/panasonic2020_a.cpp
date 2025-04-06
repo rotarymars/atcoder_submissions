51ba7ea80bcb6c3cbd521787381888fbbefb49e3
runner <runner@fv-az1991-121>
2025-04-06 14:25:12 +0000

    Add panasonic2020/panasonic2020_a/panasonic2020_a.cpp

---
+++ panasonic2020/panasonic2020_a/panasonic2020_a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    vector v{1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+    int k;
+    cin>>k;
+    cout<<v[--k]<<endl;
+    return 0;
+}
