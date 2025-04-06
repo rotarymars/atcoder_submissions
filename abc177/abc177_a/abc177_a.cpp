efc2cd4f84a6ab1189801d513c153cffe1262522
runner <runner@fv-az1991-121>
2025-04-06 14:24:57 +0000

    Add abc177/abc177_a/abc177_a.cpp

---
+++ abc177/abc177_a/abc177_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int d,t,s;
+    cin>>d>>t>>s;
+    if(d<=t*s)cout<<"Yes\n";
+    else cout<<"No\n";
+    return 0;
+}
