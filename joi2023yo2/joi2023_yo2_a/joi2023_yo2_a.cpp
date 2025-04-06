fd0bf48c4a9d9b7faca879a621ef491abc6d5890
runner <runner@fv-az1043-500>
2025-04-06 14:07:40 +0000

    Add joi2023yo2/joi2023_yo2_a/joi2023_yo2_a.cpp

--- joi2023yo2/joi2023_yo2_a/joi2023_yo2_a.cpp
+++ joi2023yo2/joi2023_yo2_a/joi2023_yo2_a.cpp
@@ -1,40 +1,22 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
 #include <vector>
-
+#include <algorithm>
+#include <cmath>
 using namespace std;
-typedef long long ll;
-
-int main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-
-    // write code here
-    int N, A_1, max, min, x, y, m;
-    //ll A_1, m, x = 0, t;
-    cin >> N;
-    vector<int> A;
-    A.reserve(N);
-    for (size_t i = 0; i < N; ++i)
-    {
-        cin >> A_1;
-        A.push_back(A_1);
+int main(){
+  int N, max, min;
+  cin >> N;
+  vector<int> A(N);
+  for(int i = 0; i < N; ++i){
+    cin >> A[i];
+  }
+  max = *max_element(A.begin(), A.end());
+  min = *min_element(A.begin(), A.end());
+  for(int i = 0; i < N; ++i){
+    if(abs(A[i] - max) > abs(A[i] - min)){
+      cout << abs(A[i] - max) << "\n";
+    }else{
+      cout << abs(A[i] - min) << "\n";
     }
-    max = *max_element(A.begin(), A.end());
-    min = *min_element(A.begin(), A.end());
-    for (size_t i = 0; i < N; ++i)
-    {
-        x = abs(A[i] - max);
-        y = abs(A[i] - min);
-        if (x > y) {
-            m = x;
-        }
-        else {
-            m = y;
-        }
-        cout << m << "\n";
-    }
-    return 0;
+  }
 }
