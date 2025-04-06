3d1ab92ce14ec1271e70358d9d0ee0cbd0b4d8ea
runner <runner@fv-az1937-505>
2025-04-06 15:08:37 +0000

    Add abc139/abc139_b/abc139_b.cpp

---
+++ abc139/abc139_b/abc139_b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  int a, b;
+  cin >> a >> b;
+  cout << (a + b - 3) / (a - 1) << endl;
+  return 0;
+}
+
+/*
+                          ./+o+-
+                  yyyyy- -yyyyyy+
+               ://+//////-yyyyyyo
+           .++ .:/++++++/-.+sss/`
+         .:++o:  /++++++++/:--:/-
+        o:+o+:++.`..```.-/oo+++++/
+       .:+o:+o/.          `+sssoo+/
+  .++/+:+oo+o:`             /sssooo.
+ /+++//+:`oo+o               /::--:.
+ \+/+o+++`o++o               ++////.
+  .++.o+++oo+:`             /dddhhh.
+       .+.o+oo:.          `oddhhhh+
+        \+.++o+o``-````.:ohdhhhhh+
+         `:o+++ `ohhhhhhhhyo++os:
+           .o:`.syhhhhhhh/.oo++o`
+               /osyyyyyyo++ooo+++/
+                   ````` +oo+++o\:
+                          `oo++.
+*/
