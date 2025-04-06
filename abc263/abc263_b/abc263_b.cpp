a308aa5de76828e523b155d5b4938d77cda48046
runner <runner@fv-az1937-505>
2025-04-06 15:08:48 +0000

    Add abc263/abc263_b/abc263_b.cpp

---
+++ abc263/abc263_b/abc263_b.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+signed main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 1; i < n; i++) {
+    cin >> a[i];
+    a[i]--;
+  }
+  vector<int> dp(n);
+  for (int i = 1; i < n; i++) {
+    dp[i] = dp[a[i]] + 1;
+  }
+  cout << dp[n - 1] << endl;
+}
+/*
+
+                   N~
+   MMMMMMMMMMMMMMM+O$M MMMMMMMMMMMMMMM+
+  :M~~~~~~~~~~~~OMOOON$M,~~~~~~~~~~~~IM
+  :M$77~~~~~~~77IMOOOOOM7I7~~~~~~~~~I8M
+     M7~~~~~~~IMOOOOOOOOM8 ~~~~~~~:IM
+     M7~~~~~~~IMOOOOOO8M +~~~~~~~IMM
+     M7~~~~~~~IMOOOOOM. ~~~~~~~DIM
+     M7~~~~~~~IMOOOMM :~~~~~~~IMM
+     M7~~~~~~~IMOOM :~~~~~~~NIM$N
+    MM7~~~~~~~IMMO ~~~~~~~~IMMOOO$M
+   7+M7~~~~~~~IM O~~~~~~~ONMOOOOOOZ$.
+ M+OOM7~~~~~~~I ~~~~~~~:IMOOOOOOOOOO$M
+ M8OOM7~~~~~~~?~~~~~~~7MMOOOOOOOOOOODM
+  88NM7~~~~~~~~~~~~~MMMOOOOOOOOOOOOD+
+    MM7~~~~~~~~~~~~M~~MOOOOOOOOOODM
+     M7~~~~~~~~~~=IMOOOOOOOOOOONN
+     M7~~~~~~~~~I?~~~MM~~~MD+~~M8~~~$
+     M7~~~~~~~IZMD~~ZOO:~~~~~~~~~~~~.
+     M7~~~~~~IMDO8~~NOM~~NDM~~M ~~~M
+     M7~~~~ZNMOOM~~8OO:~?DM8~~  M~~?
+     M7~~~IM.:ND:~~MOM~~M M~~M N~~O~
+     MMMMMM    MMMMMODNM+ ?III +III
+                 M8ODM
+                  M8O
+
+*/
