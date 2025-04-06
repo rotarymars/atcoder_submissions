069fab33f94593e60eda0196f225f1bb89595a1a
runner <runner@fv-az1043-500>
2025-04-06 14:07:32 +0000

    Add joi2023yo1a/joi2023_yo1a_d/joi2023_yo1a_d.cpp

--- joi2023yo1a/joi2023_yo1a_d/joi2023_yo1a_d.cpp
+++ joi2023yo1a/joi2023_yo1a_d/joi2023_yo1a_d.cpp
@@ -1,32 +1,11 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-
 using namespace std;
-
-int main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-
-  // write here
-  int n;
-  cin >> n;
-
-  typedef vector<int> IntVector;
-  IntVector v(n, 0);
-
-  int a;
-  for (int i = 0; i < ((2 * n) - 1); ++i) {
+int main(){
+  int N, m = 0, a;
+  cin >> N;
+  for (int i = 0; i < 2 * N - 1; ++i){
     cin >> a;
-    v[a - 1] += 1;
+    m = m + a;
   }
-
-  for (int i = 0; i < ((2 * n) - 1); ++i) {
-    if(v[i] == 1) {
-      cout << (i + 1) << "\n";
-      break;
-    }
-  }
-
-  return 0;
+  cout << (1 + N) * N - m << "\n";
 }
