e3c1062234348361942db06774515ab0b6cdabb6
runner <runner@fv-az1671-830>
2025-04-06 14:17:54 +0000

    Add abc349/abc349_c/abc349_c.cpp

--- abc349/abc349_c/abc349_c.cpp
+++ abc349/abc349_c/abc349_c.cpp
@@ -1,21 +1,40 @@
-#include <iostream>
-#include <regex>
+#include<bits/stdc++.h>
 using namespace std;
-signed main() {
-  cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-  string s, t;
-  cin >> s >> t;
-  for (auto& i : s) i = (char)(i - 32);
-  string r = "[A-Z]*" + t.substr(0, 1) + "[A-Z]*" + t.substr(1, 1) + "[A-Z]*", reee = "[A-Z]*" + t.substr(0, 1) + "[A-Z]*" + t.substr(1, 1) + "[A-Z]*" + t.substr(2, 1) + "[A-Z]*";
-  regex re(r), ree(reee);
-  if (t[2] == 'X') {
-    if (regex_match(s, re)) cout << "Yes\n";
-    else cout << "No\n";
-  }
-  else {
-    if (regex_match(s, ree)) cout << "Yes\n";
-    else cout << "No\n";
-  }
-  return 0;
+int main(){
+ string s, t;
+ cin >> s >> t;
+ int a = s.size();
+ string ans = "No";
+ int b = 0, p = 0;
+ string d = s;
+ for(int i = 0; i < 3; i++){
+   t[i] = t[i] + 'a' - 'A';
+   for(int j = p; j < a; j++){
+     if(d[j] == t[i]){
+       b++;
+       d.erase(j, 1);
+       a--;
+       p = j;
+       break;
+     }
+   }
+ } 
+ if(b == 3)ans = "Yes";
+  int e = s.size();
+ if(t[2] == 'x'){
+   int c = 0, q = 0;
+ for(int i = 0; i < 2; i++){
+   for(int j = q; j < e; j++){
+     if(s[j] == t[i]){
+       c++;
+       s.erase(j, 1);
+       e--;
+       q = j;
+       break;
+     }
+   }
+ }
+ if(c == 2)ans = "Yes";
+ }
+ cout << ans << "\n";
 }
