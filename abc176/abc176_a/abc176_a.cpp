b4d50f3f2e0102fb21da1e5f60507ee491faf706
runner <runner@fv-az1991-121>
2025-04-06 14:24:58 +0000

    Add abc176/abc176_a/abc176_a.cpp

---
+++ abc176/abc176_a/abc176_a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#ifndef __ROTARYMARS__RUD__
+#define __ROTARYMARS__RUD__
+template<class T>
+T RUD(T a, T b) {
+  return ((a + b - (T)1) / b);
+}
+#else
+#endif
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int n,x,t;
+    cin>>n>>x>>t;
+    cout<<RUD(n,x)*t<<endl;
+    return 0;
+}
