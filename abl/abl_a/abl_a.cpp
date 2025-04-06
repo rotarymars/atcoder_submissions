0a4fb5f2144a95c1beba4c4791f602de1821972f
runner <runner@fv-az1991-121>
2025-04-06 14:24:20 +0000

    Add abl/abl_a/abl_a.cpp

---
+++ abl/abl_a/abl_a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    for(;n;n--)cout<<"ACL";
+    cout<<endl;
+    return 0;
+}
