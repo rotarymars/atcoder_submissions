2a8f55b4e364e13a4b685faf4ffad09c48700c7a
runner <runner@fv-az1043-500>
2025-04-06 14:08:00 +0000

    Add math-and-algorithm/math_and_algorithm_b/math_and_algorithm_b.cpp

--- math-and-algorithm/math_and_algorithm_b/math_and_algorithm_b.cpp
+++ math-and-algorithm/math_and_algorithm_b/math_and_algorithm_b.cpp
@@ -1,18 +1,7 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
-
 using namespace std;
-
-int main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-
-    // write code here
-    int a[3];
-    cin >> a[0] >> a[1] >> a[2];
-    cout << a[0] + a[1] + a[2] << endl;
-    return 0;
+int main(){
+  int a[3];
+  cin >> a[0] >> a[1] >> a[2];
+  cout << a[0] + a[1] + a[2] << "\n";
 }
