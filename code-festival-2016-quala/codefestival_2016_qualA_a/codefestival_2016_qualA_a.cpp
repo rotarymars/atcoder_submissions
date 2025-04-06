bfad629113636b2d0d39a06f1519b52c61b3ac4b
runner <runner@fv-az1937-505>
2025-04-06 15:08:18 +0000

    Add code-festival-2016-quala/codefestival_2016_qualA_a/codefestival_2016_qualA_a.cpp

---
+++ code-festival-2016-quala/codefestival_2016_qualA_a/codefestival_2016_qualA_a.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+signed main() {
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar(' ');
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar(getchar());
+  putchar('\n');
+  return 0;
+}
