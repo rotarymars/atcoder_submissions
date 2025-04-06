81e67f8200d908cdcf3847af8a82622121b6eb59
runner <runner@fv-az1937-505>
2025-04-06 15:08:29 +0000

    Add arc018/arc018_1/arc018_1.cpp

---
+++ arc018/arc018_1/arc018_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  long double a, b;
+  cin >> a >> b;
+  a /= 100;
+  cout << b * a * a << endl;
+  return 0;
+}
+
+/*
+                          ./+o+-
+                  yyyyy- -yyyyyy+
+               ://+//////-yyyyyyo
+           .++ .:/++++++/-.+sss/`
+         .:++o:  /++++++++/:--:/-
+        o:+o+:++.`..```.-/oo+++++/
+       .:+o:+o/.          `+sssoo+/
+  .++/+:+oo+o:`             /sssooo.
+ /+++//+:`oo+o               /::--:.
+ \+/+o+++`o++o               ++////.
+  .++.o+++oo+:`             /dddhhh.
+       .+.o+oo:.          `oddhhhh+
+        \+.++o+o``-````.:ohdhhhhh+
+         `:o+++ `ohhhhhhhhyo++os:
+           .o:`.syhhhhhhh/.oo++o`
+               /osyyyyyyo++ooo+++/
+                   ````` +oo+++o\:
+                          `oo++.
+*/
