ae8899189920ee0ee011bc402ca777bd161c92e2
runner <runner@fv-az1937-505>
2025-04-06 15:08:43 +0000

    Add abc370/abc370_b/abc370_b.cpp

---
+++ abc370/abc370_b/abc370_b.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void f(int &i, int &j, vector<vector<int>> &v) {
+  int nowtmp = i;
+  int next = j;
+  if (nowtmp >= next)
+    i = v[nowtmp - 1][next - 1];
+  else
+    i = v[next - 1][nowtmp - 1];
+  return;
+}
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  int n;
+  cin >> n;
+  vector<vector<int>> v(n, vector<int>(n));
+  for (int i = 1; i <= n; i++)
+    for (int j = 0; j < i; j++)
+      cin >> v[i - 1][j];
+  int now = 1;
+  for (int i = 1; i <= n; i++)
+    f(now, i, v);
+  cout << now << endl;
+  return 0;
+}
+
+/*
+
+                   N~
+   MMMMMMMMMMMMMMM+O$M MMMMMMMMMMMMMMM+
+  :M~~~~~~~~~~~~OMOOON$M,~~~~~~~~~~~~IM
+  :M$77~~~~~~~77IMOOOOOM7I7~~~~~~~~~I8M
+     M7~~~~~~~IMOOOOOOOOM8 ~~~~~~~:IM
+     M7~~~~~~~IMOOOOOO8M +~~~~~~~IMM
+     M7~~~~~~~IMOOOOOM. ~~~~~~~DIM
+     M7~~~~~~~IMOOOMM :~~~~~~~IMM
+     M7~~~~~~~IMOOM :~~~~~~~NIM$N
+    MM7~~~~~~~IMMO ~~~~~~~~IMMOOO$M
+   7+M7~~~~~~~IM O~~~~~~~ONMOOOOOOZ$.
+ M+OOM7~~~~~~~I ~~~~~~~:IMOOOOOOOOOO$M
+ M8OOM7~~~~~~~?~~~~~~~7MMOOOOOOOOOOODM
+  88NM7~~~~~~~~~~~~~MMMOOOOOOOOOOOOD+
+    MM7~~~~~~~~~~~~M~~MOOOOOOOOOODM
+     M7~~~~~~~~~~=IMOOOOOOOOOOONN
+     M7~~~~~~~~~I?~~~MM~~~MD+~~M8~~~$
+     M7~~~~~~~IZMD~~ZOO:~~~~~~~~~~~~.
+     M7~~~~~~IMDO8~~NOM~~NDM~~M ~~~M
+     M7~~~~ZNMOOM~~8OO:~?DM8~~  M~~?
+     M7~~~IM.:ND:~~MOM~~M M~~M N~~O~
+     MMMMMM    MMMMMODNM+ ?III +III
+                 M8ODM
+                  M8O
+
+*/
