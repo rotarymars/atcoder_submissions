e03d5e6ce406ab1b2c57a3fe61f45355a8aa5fb5
runner <runner@fv-az1937-505>
2025-04-06 15:08:29 +0000

    Add abc220/abc220_c/abc220_c.cpp

---
+++ abc220/abc220_c/abc220_c.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  int n;
+  cin >> n;
+  vector<long long> v(n);
+  for (long long &i : v)
+    cin >> i;
+  for (int i = 1; i < n; i++)
+    v[i] = v[i] + v[i - 1];
+  long long ans = 0, x;
+  cin >> x;
+  ans += n * (x / v.back());
+  x %= v.back();
+  cout << 1 + ans + distance(v.begin(), upper_bound(v.begin(), v.end(), x))
+       << endl;
+  return 0;
+}
+
+/*
+                          ./+o+-
+                  yyyyy- -yyyyyy+
+               ://+//////-yyyyyyo
+           .++ .:/++++++/-.+sss/`
+         .:++o:  /++++++++/:--:/-
+        o:+o+:++.`..```.-/oo+++++/
+       .:+o:+o/.          `+sssoo+/
+  .++/+:+oo+o:`             /sssooo.
+ /+++//+:`oo+o               /::--:.
+ \+/+o+++`o++o               ++////.
+  .++.o+++oo+:`             /dddhhh.
+       .+.o+oo:.          `oddhhhh+
+        \+.++o+o``-````.:ohdhhhhh+
+         `:o+++ `ohhhhhhhhyo++os:
+           .o:`.syhhhhhhh/.oo++o`
+               /osyyyyyyo++ooo+++/
+                   ````` +oo+++o\:
+                          `oo++.
+*/
