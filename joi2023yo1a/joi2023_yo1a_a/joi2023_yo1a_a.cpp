4ce036d24bd85cad637b78f622d983db00dc7603
runner <runner@fv-az1043-500>
2025-04-06 14:07:31 +0000

    Add joi2023yo1a/joi2023_yo1a_a/joi2023_yo1a_a.cpp

---
+++ joi2023yo1a/joi2023_yo1a_a/joi2023_yo1a_a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    // write code here
+    int a, b;
+    cin >> a >> b;
+    cout << a * b << endl;
+    return 0;
+}
