bf99e41c21ad39382341c0f136cc909d5711d690
runner <runner@fv-az1937-505>
2025-04-06 15:07:04 +0000

    Add abc231/abc231_a/abc231_a.cpp

--- abc231/abc231_a/abc231_a.cpp
+++ abc231/abc231_a/abc231_a.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    long double n;
-    cin>>n;
-    cout<<n/(long double)100<<endl;
-    return 0;
+int main(){
+  double a;
+  cin>>a;
+  cout<<a/100.0<<endl;
 }
