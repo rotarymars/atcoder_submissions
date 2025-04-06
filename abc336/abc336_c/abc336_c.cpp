66615c9eb5c7dc7fded8443ad6765b6e414aa591
runner <runner@fv-az1043-500>
2025-04-06 14:08:50 +0000

    Add abc336/abc336_c/abc336_c.cpp

--- abc336/abc336_c/abc336_c.cpp
+++ abc336/abc336_c/abc336_c.cpp
@@ -1,22 +1,2 @@
 #include<bits/stdc++.h>
-using namespace std;
-
-long long multiply(long long a, long long b){
-  long long ans = 1;
-  for(long long i = 0; i < b; i++){
-    ans *= 10;
-  }
-  return ans;
-}
-
-int main(){
-  long int N;
-  cin >> N;
-  long long X = 0;
-  N--;
-  for(int i = 0; i < 19; i++){
-   X = X + multiply(1, i) * (N % 5);
-   N /= 5;
-  }
-  cout << X * 2 << "\n";
-}
+using namespace std;using A = long long;A multiply(A a, A b){A ans=1;for(A i=0;i<b;i++){ans*=10;}return ans;}int main(){A N;cin>>N;A X=0;N--;for(A i=0;i<19;i++){X=X+multiply(1,i)*(N%5);N/=5;}cout<<X*2<<"\n";}
