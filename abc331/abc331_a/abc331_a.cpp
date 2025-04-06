a924824a3e10ecbc19dd12347831f12c2d9e9043
runner <runner@fv-az1043-500>
2025-04-06 14:08:14 +0000

    Add abc331/abc331_a/abc331_a.cpp

---
+++ abc331/abc331_a/abc331_a.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+#define VALL(a) a.begin(), a.end()
+
+using LL = long long;
+using VI = std::vector<int>;
+using VVI = std::vector<VI>;
+using VC = std::vector<char>;
+using VVC = std::vector<VC>;
+
+int pt2n(const int x, const int y, const int w) { return (x * w) + y; }
+
+std::pair<int, int> num2pt(const int n, const int w) { return std::make_pair(n / w, n % w); }
+
+template <typename T>
+void PRINT1D(T a) {
+  cout << "******VECTOR 1D PRINT STARTED******\n";
+  cout << '{';
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+    cout << a[i] << ", ";
+  }
+  cout << "}\n";
+  cout << "******VECTOR 1D PRINT ENDED******\n";
+}
+
+template <typename U>
+void PRINT2D(U a) {
+  cout << "******VECTOR 2D PRINT STARTED******\n";
+  cout << "{";
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+    cout << "\n{";
+    for (size_t k = 0; k < a[i].size(); ++k)
+    {
+      cout << a[i][k] << ", ";
+    }
+    cout << "}, ";
+  }
+  cout << "\n}\n";
+  cout << "******VECTOR 2D PRINT ENDED******\n";
+}
+
+
+int main()
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  // write code here
+  int M, D, y, m, d;
+  cin >> M >> D >> y >> m >> d;
+  ++d;
+  if (d > D) {
+    ++m;
+    d = 1;
+  }
+  if (m > M) {
+    ++y;
+    m = 1;
+  }
+  cout << y << " " << m << " " << d << endl;
+  return 0;
+}
