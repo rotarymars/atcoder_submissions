22d6308ab7956c45be1bc9b7e3b5b1b7a5d6d098
runner <runner@fv-az1937-505>
2025-04-06 15:06:57 +0000

    Add abc359/abc359_a/abc359_a.cpp

--- abc359/abc359_a/abc359_a.cpp
+++ abc359/abc359_a/abc359_a.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
+#include <string>
+#include <algorithm>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int n;
-    cin>>n;
-    int takahashicnt=0,cp=n;
-    for(;cp;cp--){
-        string s;
-        cin>>s;
-        if(s=="Takahashi")takahashicnt++;
-    }
-    cout<<takahashicnt<<endl;
-    return 0;
+int main() {
+  string s="";
+  int n;
+  cin>>n;
+  for(;n;n--){
+    string tmp;
+    cin>>tmp;
+    s+=tmp;
+  }
+  cout<<count(s.begin(),s.end(),'T')<<endl;
 }
