91108b05864f2a9455f99710c8f5ba099fab3ad9
runner <runner@fv-az1671-830>
2025-04-06 14:16:09 +0000

    Add APG4b/APG4b_cs/APG4b_cs.cpp

---
+++ APG4b/APG4b_cs/APG4b_cs.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+signed main(){
+  constexpr long long sec = 365LL * 24LL * 60LL * 60LL;
+  cout << sec << "\n" << sec * 2LL << "\n" << sec * 5LL << "\n" << sec * 10LL << "\n";
+}
