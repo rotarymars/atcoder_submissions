8ca6414338be1975fbd56a3fcaf0a1044fa626f4
runner <runner@fv-az1937-505>
2025-04-06 15:07:36 +0000

    Add indeednow-quala/indeednow_2015_quala_1/indeednow_2015_quala_1.cpp

---
+++ indeednow-quala/indeednow_2015_quala_1/indeednow_2015_quala_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	string s,t;
+	cin>>s>>t;
+	cout<<s.size()*t.size()<<endl;
+	return 0;
+}
