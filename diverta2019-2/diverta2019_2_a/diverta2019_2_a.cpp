44327db0b8334733fbcb0cce28a0578da13ac6e8
runner <runner@fv-az1937-505>
2025-04-06 15:07:08 +0000

    Add diverta2019-2/diverta2019_2_a/diverta2019_2_a.cpp

---
+++ diverta2019-2/diverta2019_2_a/diverta2019_2_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int n,k;
+    cin>>n>>k;
+    if(k==1)cout<<0<<endl;
+    else cout<<n-k<<endl;
+    return 0;
+}
