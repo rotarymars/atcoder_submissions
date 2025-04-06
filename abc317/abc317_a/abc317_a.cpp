9a176b5625a88ab20122e383aa98eeda2012e41b
runner <runner@fv-az1671-830>
2025-04-06 14:18:01 +0000

    Add abc317/abc317_a/abc317_a.cpp

--- abc317/abc317_a/abc317_a.cpp
+++ abc317/abc317_a/abc317_a.cpp
@@ -1,14 +1,10 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
+int main() {
   int n, h, x;
   cin >> n >> h >> x;
-  for(int i = 0; i < n; i++){
-    int p;
-    cin >> p;
-    if(p + h >= x){
-      cout << i + 1;
-      return 0;
-    }
-  }
+  vector<int> p(n);
+  for (int &i : p) cin >> i;
+  cout << 1 + distance(p.begin(), lower_bound(p.begin(), p.end(), x - h)) << endl;
 }
