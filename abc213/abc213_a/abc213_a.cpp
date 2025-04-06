47603d2d5b617428022b9c48f752c6be22fd46c9
runner <runner@fv-az1991-121>
2025-04-06 14:24:55 +0000

    Add abc213/abc213_a/abc213_a.cpp

---
+++ abc213/abc213_a/abc213_a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int a,b;
+    cin>>a>>b;
+    for(int i=0;i<256;i++){
+        if((a^i)==b)cout<<i<<endl;
+    }
+    return 0;
+}
