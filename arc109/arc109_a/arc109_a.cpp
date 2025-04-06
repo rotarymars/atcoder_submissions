fc352927e77e3d938cd0c5f977a01ddc61ae8d8e
runner <runner@fv-az1937-505>
2025-04-06 15:08:29 +0000

    Add arc109/arc109_a/arc109_a.cpp

---
+++ arc109/arc109_a/arc109_a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+signed main() {
+  int a, b, x, y;
+  cin >> a >> b >> x >> y;
+  const int y_ = min(y, x + x);
+  a = 2 * a;
+  b = 2 * b + 1;
+  int d = abs(a - b);
+  cout << d / 2 * y_ + d % 2 * x << endl;
+  return 0;
+}
+
+/*
+                          ./+o+-
+                  yyyyy- -yyyyyy+
+               ://+//////-yyyyyyo
+           .++ .:/++++++/-.+sss/`
+         .:++o:  /++++++++/:--:/-
+        o:+o+:++.`..```.-/oo+++++/
+       .:+o:+o/.          `+sssoo+/
+  .++/+:+oo+o:`             /sssooo.
+ /+++//+:`oo+o               /::--:.
+ \+/+o+++`o++o               ++////.
+  .++.o+++oo+:`             /dddhhh.
+       .+.o+oo:.          `oddhhhh+
+        \+.++o+o``-````.:ohdhhhhh+
+         `:o+++ `ohhhhhhhhyo++os:
+           .o:`.syhhhhhhh/.oo++o`
+               /osyyyyyyo++ooo+++/
+                   ````` +oo+++o\:
+                          `oo++.
+*/
