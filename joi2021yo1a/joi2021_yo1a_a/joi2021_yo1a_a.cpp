a6f7e66a4cc9771cb8977ca96aae23dbda29d1c5
runner <runner@fv-az1043-500>
2025-04-06 14:07:44 +0000

    Add joi2021yo1a/joi2021_yo1a_a/joi2021_yo1a_a.cpp

---
+++ joi2021yo1a/joi2021_yo1a_a/joi2021_yo1a_a.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+using namespace std;
+int main(){
+  vector<int> a(3);
+  cin >> a[0] >> a[1] >> a[2];
+  sort(a.begin(), a.end());
+  cout << a[1] << endl;
+  return 0;
+}
