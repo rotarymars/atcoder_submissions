fcfd3960298fff215847c7af01f259810ee9ea6d
runner <runner@fv-az1043-500>
2025-04-06 14:07:48 +0000

    Add APG4b/APG4b_cv/APG4b_cv.cpp

---
+++ APG4b/APG4b_cv/APG4b_cv.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    // write code here
+    cout << "こんにちは\nAtCoder\n";
+    return 0;
+}
