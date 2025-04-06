ff4f4aaa87c34bb5159d01ef86bd31ea78ab562c
runner <runner@fv-az1043-500>
2025-04-06 14:08:48 +0000

    Add practice2/practice2_a/practice2_a.cpp

--- practice2/practice2_a/practice2_a.cpp
+++ practice2/practice2_a/practice2_a.cpp
@@ -1,106 +1,25 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
-
-
-
-#include <algorithm>
-#include <cassert>
-#include <vector>
-
-namespace ALG {
-
-// Implement (union by size) + (path compression)
-// Reference:
-// Zvi Galil and Giuseppe F. Italiano,
-// Data structures and algorithms for disjoint set union problems
-struct dsu {
-  public:
-    dsu() : _n(0) {}
-    explicit dsu(int n) : _n(n), parent_or_size(n, -1) {}
-
-    int merge(int a, int b) {
-        assert(0 <= a && a < _n);
-        assert(0 <= b && b < _n);
-        int x = leader(a), y = leader(b);
-        if (x == y) return x;
-        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
-        parent_or_size[x] += parent_or_size[y];
-        parent_or_size[y] = x;
-        return x;
-    }
-
-    bool same(int a, int b) {
-        assert(0 <= a && a < _n);
-        assert(0 <= b && b < _n);
-        return leader(a) == leader(b);
-    }
-
-    int leader(int a) {
-        assert(0 <= a && a < _n);
-        if (parent_or_size[a] < 0) return a;
-        return parent_or_size[a] = leader(parent_or_size[a]);
-    }
-
-    int size(int a) {
-        assert(0 <= a && a < _n);
-        return -parent_or_size[leader(a)];
-    }
-
-    std::vector<std::vector<int>> groups() {
-        std::vector<int> leader_buf(_n), group_size(_n);
-        for (int i = 0; i < _n; i++) {
-            leader_buf[i] = leader(i);
-            group_size[leader_buf[i]]++;
-        }
-        std::vector<std::vector<int>> result(_n);
-        for (int i = 0; i < _n; i++) {
-            result[i].reserve(group_size[i]);
-        }
-        for (int i = 0; i < _n; i++) {
-            result[leader_buf[i]].push_back(i);
-        }
-        result.erase(
-            std::remove_if(result.begin(), result.end(),
-                           [&](const std::vector<int>& v) { return v.empty(); }),
-            result.end());
-        return result;
-    }
-
-  private:
-    int _n;
-    // root node: -1 * component size
-    // otherwise: parent
-    std::vector<int> parent_or_size;
-};
-
-}  // namespace atcoder
-
-
+#include <bits/stdc++.h>
+#include <atcoder/dsu>
 using namespace std;
-
-int main()
-{
-  cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-
-  // write code here
-  int n, q, t, u, v;
+int main(){
+  int n, q;
   cin >> n >> q;
-  ALG::dsu a(n);
-  for (size_t i = 0; i < q; ++i)
-  {
-    cin >> t >> u >> v;
-    if (t) {
-      if (a.same(u, v)) {
+  atcoder::dsu d(n);
+  for(int i = 0; i < q; i++){
+    static int t;
+    cin >> t;
+    if(t == 0){
+      static int u, v;
+      cin >> u >> v;
+      d.merge(u, v);
+    }else{
+      static int u, v;
+      cin >> u >> v;
+      if(d.same(u, v)){
         cout << "1\n";
+      }else{
+        cout << "0\n";
       }
-      else cout << "0\n";
-    }
-    else {
-      a.merge(u, v);
     }
   }
-  return 0;
 }
