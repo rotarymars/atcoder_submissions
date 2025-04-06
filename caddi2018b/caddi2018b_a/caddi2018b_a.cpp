cd271c6f6e184e3b2004804681857e7eaba2740e
runner <runner@fv-az1937-505>
2025-04-06 15:07:02 +0000

    Add caddi2018b/caddi2018b_a/caddi2018b_a.cpp

---
+++ caddi2018b/caddi2018b_a/caddi2018b_a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    string s;
+    cin>>s;
+    cout<<count(s.begin(),s.end(),'2')<<endl;
+    return 0;
+}
