9d6061f023d328962d89e23587380971c8cb3d56
runner <runner@fv-az1937-505>
2025-04-06 15:09:06 +0000

    Add arc146/arc146_a/arc146_a.cpp

---
+++ arc146/arc146_a/arc146_a.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) cin >> a[i];
+  sort(rbegin(a), rend(a));
+  vector<string> b{to_string(a[0]), to_string(a[1]), to_string(a[2])};
+  vector<int> x{0, 1, 2};
+  unsigned long long m = 0;
+  do {
+    unsigned long long v = stol(b[x[0]] + b[x[1]] + b[x[2]]);
+    m = max(m, v);
+  } while (next_permutation(begin(x), end(x)));
+  cout << m << endl;
+}
+/*
+
+                   N~
+   MMMMMMMMMMMMMMM+O$M MMMMMMMMMMMMMMM+
+  :M~~~~~~~~~~~~OMOOON$M,~~~~~~~~~~~~IM
+  :M$77~~~~~~~77IMOOOOOM7I7~~~~~~~~~I8M
+     M7~~~~~~~IMOOOOOOOOM8 ~~~~~~~:IM
+     M7~~~~~~~IMOOOOOO8M +~~~~~~~IMM
+     M7~~~~~~~IMOOOOOM. ~~~~~~~DIM
+     M7~~~~~~~IMOOOMM :~~~~~~~IMM
+     M7~~~~~~~IMOOM :~~~~~~~NIM$N
+    MM7~~~~~~~IMMO ~~~~~~~~IMMOOO$M
+   7+M7~~~~~~~IM O~~~~~~~ONMOOOOOOZ$.
+ M+OOM7~~~~~~~I ~~~~~~~:IMOOOOOOOOOO$M
+ M8OOM7~~~~~~~?~~~~~~~7MMOOOOOOOOOOODM
+  88NM7~~~~~~~~~~~~~MMMOOOOOOOOOOOOD+
+    MM7~~~~~~~~~~~~M~~MOOOOOOOOOODM
+     M7~~~~~~~~~~=IMOOOOOOOOOOONN
+     M7~~~~~~~~~I?~~~MM~~~MD+~~M8~~~$
+     M7~~~~~~~IZMD~~ZOO:~~~~~~~~~~~~.
+     M7~~~~~~IMDO8~~NOM~~NDM~~M ~~~M
+     M7~~~~ZNMOOM~~8OO:~?DM8~~  M~~?
+     M7~~~IM.:ND:~~MOM~~M M~~M N~~O~
+     MMMMMM    MMMMMODNM+ ?III +III
+                 M8ODM
+                  M8O
+
+*/
