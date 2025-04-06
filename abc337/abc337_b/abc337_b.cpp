4f8b68ee2cfb4fb879b0a7ce17a2463e0147b2b1
runner <runner@fv-az1671-830>
2025-04-06 14:18:02 +0000

    Add abc337/abc337_b/abc337_b.cpp

--- abc337/abc337_b/abc337_b.cpp
+++ abc337/abc337_b/abc337_b.cpp
@@ -6,6 +6,5 @@ int main() {
   string s;
   cin >> s;
   regex r("^A*B*C*$");
-  if (regex_match(s, r)) cout << "Yes\n";
-  else cout << "No\n";
+  cout << (regex_match(s, r) ? "Yes\n" : "No\n");
 }
