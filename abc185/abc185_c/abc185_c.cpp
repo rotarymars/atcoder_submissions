c8537408abf3618e9093e71a8251aeb80d141a93
runner <runner@fv-az1341-354>
2025-04-07 15:10:06 +0000

    Add abc185/abc185_c/abc185_c.cpp

--- abc185/abc185_c/abc185_c.cpp
+++ abc185/abc185_c/abc185_c.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#ifndef __ROTARYMARS__COMBINATIONS__
+#define __ROTARYMARS__COMBINATIONS__
+template<class T>
+T COMBINATIONS(T a, T b){
+  T ret=1;
+  for(T i=1;i<=b;++i){
+    ret*=a-i;
+    ret/=i;
+  }
+  return ret;
+}
+#endif
 using namespace std;
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
   long long n;
   cin>>n;
-  long long ans=1;
-  for(int i=1;i<=11;++i){
-    ans*=n-i;
-    ans/=i;
-  }
-  cout<<ans<<endl;
+  cout<<COMBINATIONS(n,11ll)<<endl;
   return 0;
 }
