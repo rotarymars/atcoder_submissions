a03ece2f7454257b65f7333937a0990797b08558
runner <runner@fv-az1341-354>
2025-04-07 15:11:46 +0000

    Add abc237/abc237_a/abc237_a.cpp

--- abc237/abc237_a/abc237_a.cpp
+++ abc237/abc237_a/abc237_a.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-#include <limits.h>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    long long n;
-    cin>>n;
-    if(n>=INT_MIN&&n<=INT_MAX)cout<<"Yes\n";
-    else cout<<"No\n";
-    return 0;
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  long long a;
+  cin>>a;
+  constexpr signed long long lim=1ll<<31;
+  if(-lim<=a&&a<lim){
+    cout<<"Yes\n";
+  }
+  else{
+    cout<<"No\n";
+  }
+  return 0;
 }
