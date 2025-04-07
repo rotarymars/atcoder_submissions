15016b67606ec74517e09cac5caf3ee3159b1921
runner <runner@fv-az1341-354>
2025-04-07 15:11:45 +0000

    Add abc237/abc237_b/abc237_b.cpp

--- abc237/abc237_b/abc237_b.cpp
+++ abc237/abc237_b/abc237_b.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int h,w;
-    cin>>h>>w;
-    vector<vector<int>>v(h,vector<int>(w));
-    for(int i=0;i<h;i++)for(int j=0;j<w;j++)cin>>v[i][j];
-    for(int i=0;i<w;i++){
-        for(int j=0;j<h;j++){
-        if(j==0)cout<<v[j][i];
-        else cout<<' '<<v[j][i];
-        }
-        cout<<endl;
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int h,w;
+  cin>>h>>w;
+  vector<vector<int>>a(h,vector<int>(w)),b(w,vector<int>(h));
+  for(auto&i:a){
+    for(auto&j:i){
+      cin>>j;
     }
-    return 0;
+  }
+  for(int i=0;i<h;++i){
+    for(int j=0;j<w;++j){
+      b[j][i]=a[i][j];
+    }
+  }
+  for(int i=0;i<w;++i){
+    for(int j=0;j<h;++j){
+      cout<<(j==0?"":" ")<<b[i][j];
+    }
+    cout<<'\n';
+  }
+  return 0;
 }
