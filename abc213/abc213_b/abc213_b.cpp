f2a806f8a151cdea091a255a43886698a947c779
runner <runner@fv-az1341-354>
2025-04-07 15:09:56 +0000

    Add abc213/abc213_b/abc213_b.cpp

--- abc213/abc213_b/abc213_b.cpp
+++ abc213/abc213_b/abc213_b.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
+#include <utility>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int n;
-    cin>>n;
-    vector<int>v(n);
-    for(int&i:v)cin>>i;
-    vector vcopy=v;
-    sort(v.begin(),v.end());
-    cout<<distance(vcopy.begin(),find(vcopy.begin(),vcopy.end(),*prev(v.end(),2)))+1<<endl;
-    return 0;
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n;
+  cin>>n;
+  vector<pair<int,int>>v(n);
+  for(int i=0;i<n;i++){
+    cin>>v[i].first;
+    v[i].second=i+1;
+  }
+  sort(v.begin(),v.end(),greater<>());
+  cout<<v[1].second<<endl;
+  return 0;
 }
