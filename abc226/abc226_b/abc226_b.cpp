7bc1c265757ed5346aa607c6fb31eda3a9cca981
runner <runner@fv-az1341-354>
2025-04-07 15:09:39 +0000

    Add abc226/abc226_b/abc226_b.cpp

---
+++ abc226/abc226_b/abc226_b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n;
+  cin >> n;
+  set<vector<int>> se;
+  for (; n; n--) {
+    int l;
+    cin >> l;
+    vector<int> v(l);
+    for (int &i : v)
+      cin >> i;
+    se.insert(v);
+  }
+  cout << se.size() << endl;
+}
