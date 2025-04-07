724f14bd0a6e9d8c492965718748e7af39785740
runner <runner@fv-az1341-354>
2025-04-07 15:10:40 +0000

    Add abc389/abc389_a/abc389_a.cpp

---
+++ abc389/abc389_a/abc389_a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  char a,b;
+  cin>>a>>b>>b;
+  cout<<(int)(a-'0')*(int)(b-'0')<<endl;
+  return 0;
+}
