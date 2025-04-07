b4b46ab8fb9b10949bf42793ab3f5cee772ef3b2
runner <runner@fv-az1341-354>
2025-04-07 15:10:19 +0000

    Add abc171/abc171_a/abc171_a.cpp

--- abc171/abc171_a/abc171_a.cpp
+++ abc171/abc171_a/abc171_a.cpp
@@ -1,36 +1,12 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
-bool ISUPPER(char a)
-{
-    if (a >= 'A' && a <= 'Z')
-    {
-        return true;
-    }
-    return false;
-}
-bool ISLOWER(char a)
-{
-    if (a >= 'a' && a <= 'z')
-    {
-        return true;
-    }
-    return false;
-}
-char TOUPPER(char a)
-{
-    return (char)(a - 32);
-}
-char TOLOWER(char a)
-{
-    return (char)(a + 32);
-}
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    char a;
-    cin>>a;
-    if(ISUPPER(a))cout<<"A"<<endl;
-    else cout<<"a"<<endl;
-    return 0;
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  char c;
+  cin>>c;
+  if(isupper(c))cout<<'A'<<endl;
+  else cout<<'a'<<endl;
+  return 0;
 }
