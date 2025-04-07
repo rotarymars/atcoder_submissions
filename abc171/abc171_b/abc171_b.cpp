ca4ca1f46a2f6502351e7852fa02654264dbce05
runner <runner@fv-az1341-354>
2025-04-07 15:10:18 +0000

    Add abc171/abc171_b/abc171_b.cpp

--- abc171/abc171_b/abc171_b.cpp
+++ abc171/abc171_b/abc171_b.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-#include <numeric>
 #include <vector>
 #include <algorithm>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
+#include <numeric>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int n,k;
-    cin>>n>>k;
-    vector<int>v(n);
-    for(int&i:v)cin>>i;
-    sort(v.begin(),v.end());
-    cout<<accumulate(v.begin(),next(v.begin(),k),0)<<endl;
-    return 0;
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n,k;
+  cin>>n>>k;
+  vector<int>v(n);
+  for(auto&i:v)cin>>i;
+  sort(v.begin(),v.end());
+  cout<<accumulate(v.begin(),next(v.begin(),k),0)<<endl;
+  return 0;
 }
