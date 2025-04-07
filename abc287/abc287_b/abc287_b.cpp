55aebe6b738aab453f1c8250c3ea69347b7f6435
runner <runner@fv-az1341-354>
2025-04-07 15:09:46 +0000

    Add abc287/abc287_b/abc287_b.cpp

--- abc287/abc287_b/abc287_b.cpp
+++ abc287/abc287_b/abc287_b.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int n,m;
-    cin>>n>>m;
-    vector<string>s,t;
-    s.reserve(n);t.reserve(m);
-    for(;n;n--){
-        string tmp;
-        cin>>tmp;
-        s.emplace_back(tmp.substr(tmp.size()-3,3));
-    }
-    for(;m;m--){
-        string tmp;
-        cin>>tmp;
-        t.emplace_back(tmp);
-    }
-    int ans=0;
-    for(string&i:s){
-        if(find(t.begin(),t.end(),i)!=t.end())ans++;
-    }
-    cout<<ans<<endl;
-    #ifdef _DEBUG
-    for(string&i:s)cout<<i<<' ';
-    cout<<endl;
-    for(string&i:t)cout<<i<<' ';
-    cout<<endl;
-    #endif
-    return 0;
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n,m;
+  cin>>n>>m;
+  vector<string>v;
+  set<string>se;
+  for(int i=0;i<n;i++){
+    string s;
+    cin>>s;
+    v.push_back(s.substr(s.size()-3,3));
+  }
+  for(int i=0;i<m;i++){
+    string s;
+    cin>>s;
+    se.insert(s);
+  }
+  int ans=0;
+  for(auto&i:v)ans+=se.contains(i);
+  cout<<ans<<endl;
+  return 0;
 }
