8da35fbfa731ee7433c4b6cdf774f52417494f9e
runner <runner@fv-az1341-354>
2025-04-07 15:10:21 +0000

    Add tessoku-book/tessoku_book_m/tessoku_book_m.cpp

--- tessoku-book/tessoku_book_m/tessoku_book_m.cpp
+++ tessoku-book/tessoku_book_m/tessoku_book_m.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <iterator>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int n,k;
-    cin>>n>>k;
-    vector<int>v(n);
-    for(int&i:v)cin>>i;
-    long long ans=0;
-    for(int i=0;i<n;i++){
-        auto it=lower_bound(v.begin(),v.end(),v[i]+k);
-        if(*it!=v[i]+k)it--;
-        ans+=distance(next(v.begin(),i),it);
+using ll=long long;
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n,k;
+  cin>>n>>k;
+  vector<int>v(n);
+  for(auto&i:v)cin>>i;
+  sort(v.begin(),v.end());
+  int lind=0,rind=0;
+  ll ans=0;
+  while(lind<n){
+    if(rind<n)if(v[rind]-v[lind]<=k){
+      ++rind;
+      continue;
     }
-    cout<<ans<<endl;
-    return 0;
+    ans+=rind-lind-1;
+    ++lind;
+  }
+  cout<<ans<<endl;
+  return 0;
 }
