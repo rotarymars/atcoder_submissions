8b0bfcc51c7c47def6a9419f558198bfe22db0e9
runner <runner@fv-az1951-803>
2025-04-13 15:06:59 +0000

    Add abc401/abc401_a/abc401_a.cpp 2025-04-12 21:01:08

---
+++ abc401/abc401_a/abc401_a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n;
+  cin>>n;
+  if(200<=n&&n<=299){
+    cout<<"Success\n";
+  }
+  else{
+    cout<<"Failure\n";
+  }
+  return 0;
+}
