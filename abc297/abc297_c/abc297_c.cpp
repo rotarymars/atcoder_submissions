e3e33711730f24ee75f0c572b8ea609beb573bb9
runner <runner@fv-az2249-3>
2025-05-03 15:07:17 +0000

    Add abc297/abc297_c/abc297_c.cpp 2025-05-03 18:37:38

--- abc297/abc297_c/abc297_c.cpp
+++ abc297/abc297_c/abc297_c.cpp
@@ -1,53 +1,30 @@
 #include <iostream>
-#ifndef __ROTARYMARS__VECTORIO__
-#define __ROTARYMARS__VECTORIO__
-#include <iostream>
-#include <vector>
-template <class T>
-std::istream &operator>>(std::istream &in, std::vector<T> &v) {
-  for (T &i : v)
-    in >> i;
-  return in;
-}
-template <class T>
-std::ostream &operator<<(std::ostream &out, std::vector<T> &v) {
-  for (T &i : v)
-    out << i << ' ';
-  return out;
-}
-#else
-#endif
-#ifndef __ROTARYMARS__DEBUGPRINT__
-#define __ROTARYMARS__DEBUGPRINT__
-#include <iostream>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
-#else
-#endif
+#include <string>
 #include <vector>
 using namespace std;
+void f(string*s){
+  for(int i=0;i<s->size()-1;i++){
+    if((*s)[i]=='T'&&(*s)[i+1]=='T'){
+      (*s)[i]='P';
+      (*s)[i+1]='C';
+      i++;
+    }
+  }
+}
 signed main() {
   cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-  int h, w;
-  cin >> h >> w;
-  vector<vector<char>> v(h, vector<char>(w));
-  for (auto &i : v)
-    for (char &j : i)
-      cin >> j;
-  for (int i = 0; i < h; i++)
-    for (int j = 0; j < w - 1; j++) {
-      if (v[i][j] == v[i][j + 1] && v[i][j] == 'T')
-        v[i][j] = 'P', v[i][j + 1] = 'C';
-      DP(v);
-    }
-  for (auto &i : v) {
-    for (char &j : i)
-      cout << j;
-    cout << '\n';
+  ios::sync_with_stdio(false);
+  int h,w;
+  cin>>h>>w;
+  vector<string>v(h);
+  for(auto&i:v){
+    cin>>i;
+  }
+  for(int i=0;i<h;i++){
+    f(&v[i]);
+  }
+  for(auto&i:v){
+    cout<<i<<endl;
   }
   return 0;
 }
