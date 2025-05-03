cbe34182d61f2da76e524bb1f7d509711fcd8372
runner <runner@fv-az2249-3>
2025-05-03 15:07:18 +0000

    Add abc297/abc297_b/abc297_b.cpp 2025-05-03 18:43:58

--- abc297/abc297_b/abc297_b.cpp
+++ abc297/abc297_b/abc297_b.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
-#include <regex>
-#include <utility>
 #include <string>
+#include <regex>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    string s;
-    cin>>s;
-    pair<int,int>p(-1,-1);
-    for(int i=0;i<8;i++){
-        if(s[i]=='B'){
-            if(p.first==-1)p.first=i;
-            else {
-                p.second=i;
-                break;
-            }
-        }
-    }
-    if((p.first^p.second)&1)goto label;
-    cout<<"No\n";
-    return 0;
-    label:
-    if(regex_match(s,regex(".*R.*K.*R.*"))){
-        cout<<"Yes\n";
-        return 0;
-    }
-    cout<<"No\n";
-    return 0;
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  string s;
+  cin>>s;
+  regex r("^.*R.*K.*R.*$");
+  auto it=find(s.begin(),s.end(),'B');
+  auto itt=find(it+1,s.end(),'B');
+  if(regex_match(s,r) && ((distance(s.begin(),it)^distance(s.begin(),itt))&1)){
+    cout<<"Yes"<<endl;
+  }else{
+    cout<<"No"<<endl;
+  }
+  return 0;
 }
