b615b2fd70304ff96aa484318045857fa04946d1
runner <runner@fv-az2249-3>
2025-05-03 15:07:15 +0000

    Add abc297/abc297_d/abc297_d.cpp 2025-05-03 18:00:11

---
+++ abc297/abc297_d/abc297_d.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  long long a,b;
+  cin>>a>>b;
+  long long ans=0;
+  if(a<b) swap(a,b);
+  while(b>0){
+    ans+=a/b;
+    a%=b;
+    swap(a,b);
+  }
+  cout<<ans-1<<endl;
+}
