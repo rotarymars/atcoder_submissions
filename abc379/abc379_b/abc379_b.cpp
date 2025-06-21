ffd71aa3e36d18ab1aa27e518289fd8de715745f
runner <runner@fv-az1914-629>
2025-06-21 15:07:50 +0000

    Add abc379/abc379_b/abc379_b.cpp 2025-06-21 20:30:08

--- abc379/abc379_b/abc379_b.cpp
+++ abc379/abc379_b/abc379_b.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
-signed main() {
-  cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-  int n, k;
-  cin >> n >> k;
-  string s;
-  cin >> s;
-  int ans = 0;
-  for (int i = 0; i < n - k + 1; ++i) {
-    if (s.substr(i, k) == string(k, 'O')) {
-      ++ans;
-      for (int j = i; j < i + k; ++j) s[j] = 'X';
+int times=0;
+void compute(string&s,int i, int k){
+  if(s.substr(i,k)==string(k,'O')){
+    ++times;
+    for(int j=i;j<i+k;++j){
+      s[j]='X';
     }
   }
-  cout << ans << endl;
+  return;
+}
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n,k;
+  string s;
+  cin>>n>>k>>s;
+  for(int i=0;i<n-k+1;++i){
+    compute(s,i,k);
+  }
+  cout<<times<<endl;
   return 0;
 }
