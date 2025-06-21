ad838fa906705aa8457033ec55a6cbbcdf304ee5
runner <runner@fv-az1914-629>
2025-06-21 15:07:52 +0000

    Add abc379/abc379_a/abc379_a.cpp 2025-06-21 20:32:28

--- abc379/abc379_a/abc379_a.cpp
+++ abc379/abc379_a/abc379_a.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <string>
 using namespace std;
 signed main() {
   cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-  string s;
-  cin >> s;
-  cout << s[1] << s[2] << s[0] << ' ' << s[2] << s[0] << s[1] << endl;
+  ios::sync_with_stdio(false);
+  int n;
+  cin>>n;
+  int c=n%10;
+  n/=10;
+  int b=n%10;
+  n/=10;
+  int a=n%10;
+  n/=10;
+  cout<<100*b+10*c+a<<' '<<100*c+10*a+b<<endl;
   return 0;
 }
