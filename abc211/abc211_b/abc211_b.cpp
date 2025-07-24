ab3f3e958cf6e689ee6e3b7a9395fee837e346e9
runner <runner@pkrvmpptgkbjq6m>
2025-07-24 15:09:59 +0000

    Add abc211/abc211_b/abc211_b.cpp 2025-07-24 19:27:33

--- abc211/abc211_b/abc211_b.cpp
+++ abc211/abc211_b/abc211_b.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 #include <set>
 #include <string>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
 using namespace std;
-string I(){
-    string s;
-    cin>>s;
-    return s;
-}
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    set s{I(),I(),I(),I()};
-    if(s.size()==4)cout<<"Yes\n";
-    else cout<<"No\n";
-    return 0;
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  string sa,sb,sc,sd;
+  cin>>sa>>sb>>sc>>sd;
+  set<string>st;
+  st.insert(sa);
+  st.insert(sb);
+  st.insert(sc);
+  st.insert(sd);
+  if(st.size()==4)cout<<"Yes\n";
+  else cout<<"No\n";
+  return 0;
 }
