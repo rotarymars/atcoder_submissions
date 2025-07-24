816972a85b90a42f19c1ec5772355691c95ee7aa
runner <runner@pkrvmpptgkbjq6m>
2025-07-24 15:09:54 +0000

    Add abc204/abc204_b/abc204_b.cpp 2025-07-24 19:06:59

--- abc204/abc204_b/abc204_b.cpp
+++ abc204/abc204_b/abc204_b.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    int ans=0;
-    for(;n;n--){
-        int tmp;
-        cin >> tmp;
-        ans+=(tmp>=10?tmp-10:0);
-    }
-    cout<<ans<<endl;
-    return 0;
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n;
+  cin>>n;
+  int ans=0;
+  for(int i=0;i<n;++i){
+    int a;
+    cin>>a;
+    if(a>10)ans+=a-10;
+  }
+  cout<<ans<<endl;
+  return 0;
 }
