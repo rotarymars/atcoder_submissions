019b339ee4a9835383b6abaa70ca731a142e4929
runner <runner@pkrvmpptgkbjq6m>
2025-07-24 15:09:47 +0000

    Add abc182/abc182_b/abc182_b.cpp 2025-07-24 11:39:09

--- abc182/abc182_b/abc182_b.cpp
+++ abc182/abc182_b/abc182_b.cpp
@@ -1,28 +1,29 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 signed main() {
   cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
+  ios::sync_with_stdio(false);
   int n;
-  cin >> n;
-  vector<int> v(n);
-  for (int &i : v)
-    cin >> i;
-  int a = *max_element(v.begin(), v.end());
-  int ans = 0, cnt = 0;
-  for (int i = 2; i <= a; i++) {
-    int tmp = 0;
-    for (int j = 0; j < n; j++) {
-      if (v[j] % i == 0)
-        tmp++;
+  cin>>n;
+  vector<int>v(n);
+  for(int i=0;i<n;++i){
+    cin>>v[i];
+  }
+  int maxcnt=-1;
+  int ans=-1;
+  const int maxv=*max_element(v.begin(),v.end());
+  for(int i=2;i<=maxv;++i){
+    int cnt=0;
+    for(int j=0;j<n;++j){
+      if(v[j]%i==0)++cnt;
     }
-    if (cnt < tmp) {
-      cnt = tmp;
-      ans = i;
+    if(cnt>maxcnt){
+      ans=i;
+      maxcnt=cnt;
     }
   }
-  cout << ans << endl;
+  cout<<ans<<'\n';
   return 0;
 }
