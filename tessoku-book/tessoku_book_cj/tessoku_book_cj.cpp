4dbd70bb95f0cc83282b2d89a5dd8f219db9d54b
runner <runner@runnervmg1sw1>
2025-11-29 15:08:18 +0000

    Add tessoku-book/tessoku_book_cj/tessoku_book_cj.cpp 2025-11-29 18:06:09

--- tessoku-book/tessoku_book_cj/tessoku_book_cj.cpp
+++ tessoku-book/tessoku_book_cj/tessoku_book_cj.cpp
@@ -1,27 +1,22 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int n;
-    cin>>n;
-    vector<int>v(n);
-    for(int&i:v)cin>>i;
-    sort(v.begin(),v.end());
-    int q;
-    cin>>q;
-    for(;q;q--){
-        int x;
-        cin>>x;
-        cout<<distance(v.begin(),lower_bound(v.begin(),v.end(),x))<<endl;
-    }
-    return 0;
+signed main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (auto &i : v) {
+    cin >> i;
+  }
+  sort(v.begin(), v.end());
+  int q;
+  cin >> q;
+  while (q--) {
+    int x;
+    cin >> x;
+    cout << distance(v.begin(), lower_bound(v.begin(), v.end(), x)) << '\n';
+  }
+  return 0;
 }
